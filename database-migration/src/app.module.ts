4f8670bc8a7f21553c53e162341f63bb91b4b5df
Jh0nC <jhonatancorrales681@gmail.com>
2025-04-14 06:31:32 -0500

    config env variables
    Created a function that load enviroment variables from .env to app

--- database-migration/src/app.module.ts
+++ database-migration/src/app.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { envVariables } from './config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      logging: true,
+      synchronize: true,
+      host: envVariables.dbHost,
+      port: envVariables.dbPort,
+      username: envVariables.dbUsername,
+      password: envVariables.dbPassword,
+      database: envVariables.dbName,
+    }),
+    DatabaseModule,
+  ],
   controllers: [],
   providers: [],
 })
