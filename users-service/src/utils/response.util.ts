3ace91c3d1be1e58c78ee5b7a7d76b7e80f377a0
Jh0nC <jhonatancorrales681@gmail.com>
2025-05-28 02:07:39 -0500

    Update response file

--- users-service/src/utils/response.util.ts
+++ users-service/src/utils/response.util.ts
@@ -1,28 +1,27 @@
-import { Logger } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  InternalServerErrorException,
+  NotFoundException,
+  Logger,
+} from '@nestjs/common';
 
 export const notFoundResponse = (reqAttribute: string) => {
-  return {
+  throw new NotFoundException({
     statusCode: 404,
     message: `Request data, ${reqAttribute}, was not found`,
     error: 'Not Found',
-  };
+  });
 };
 
 export const errorResponse = (error: any, message?: string) => {
   Logger.error(`\nError: ${message || 'Unexpected error'}`, error.stack);
-  return {
+
+  throw new InternalServerErrorException({
     statusCode: 500,
     message: message || 'Unexpected error',
     error: error.message || String(error),
-  };
-};
-
-export const successResponse = (data: any, message?: string) => {
-  return {
-    statusCode: 200,
-    message: message || 'Successful operation',
-    data: data,
-  };
+  });
 };
 
 export const conflictResponse = (
@@ -31,9 +30,10 @@ export const conflictResponse = (
   message?: string,
 ) => {
   Logger.error(
-    `Conflict - Attribute: ${attribute || 'N/A'}, Details: ${conflict || 'N/A'}, Message: ${message || 'There\'s a conflict in the operation.'}`,
+    `Conflict - Attribute: ${attribute || 'N/A'}, Details: ${conflict || 'N/A'}, Message: ${message || "There's a conflict in the operation."}`,
   );
-  return {
+
+  throw new ConflictException({
     statusCode: 409,
     message: attribute
       ? `Attribute: ${attribute} is generating some conflict`
@@ -41,16 +41,17 @@ export const conflictResponse = (
         ? message
         : `There's a conflict in the operation. Please review the details.`,
     conflict: conflict || 'Conflict',
-  };
+  });
 };
 
 export const emptyDataResponse = (attributes: string | string[]) => {
   const attributesString = Array.isArray(attributes)
     ? attributes.join(', ')
     : attributes;
-  return {
+
+  throw new BadRequestException({
     statusCode: 400,
     message: `No valid fields were provided for update. Please include at least one of the following attributes: ${attributesString}`,
     error: 'Bad Request',
-  };
+  });
 };
